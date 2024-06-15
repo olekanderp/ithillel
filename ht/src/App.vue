f5e54a8a408f8057e1dc2a7f05486dd61d7cc8d4
Oleksandr Palaniuk <oleksandr.p@aquimarketing.com>
2024-06-15 14:45:12 +0300

    fisrt

--- ht/src/App.vue
+++ ht/src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <Carousel :images="listImages">
-
-  </Carousel>
+  <BtnGroup>
+  </BtnGroup>
 </template>
 
 <script>
-import Carousel from './components/Carousel.vue'
+import BtnGroup from './components/BtnGroup.vue'
 
 
 export default {
   name: 'App',
   components: {
-    Carousel
+    BtnGroup
   },
   data() {
     return {
     };
   },
   computed: {
-    listImages() {
-     return ['assets/images/first.jpg' ,'assets/images/second.jpg', 'assets/images/third.jpg'];
-    }
   },
 
 }
---
+++ ht/src/components/BtnGroup.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="btn-group" role="group">
+    <button type="button" class="btn btn-secondary left" :class="{ active: activeItemFisrt }"  @click="selectButton(1)">Left</button>
+    <button type="button" class="btn btn-secondary right" :class="{ active: activeItemSecond }" @click="selectButton(2)">Right</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BtnGroup",
+  data(){
+    return {
+      activeItemFisrt: false,
+      activeItemSecond: false
+    }
+  },
+  methods:{
+    selectButton(index){
+      if (index == 1){
+        this.activeItemFisrt = true
+        this.activeItemSecond = false
+      }else {
+        this.activeItemFisrt = false
+        this.activeItemSecond = true
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
