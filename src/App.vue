83b2b32f224ea9f3f63322bb7eb9125cac1d33e3
Oleksandr Palaniuk <oleksandr.p@aquimarketing.com>
2024-07-28 15:15:54 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,3 @@
 <template>
-  <div id="app">
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container">
-        <div class="collapse navbar-collapse">
-          <ul class="navbar-nav mx-auto mb-2 mb-lg-0 d-flex justify-content-center">
-            <li class="nav-item">
-              <router-link :to="{ name: 'home' }" class="nav-link">
-                <button type="button" class="btn btn-primary">Home</button>
-              </router-link>
-            </li>
-            <li class="nav-item">
-              <router-link :to="{ name: 'add-article' }" class="nav-link">
-                <button type="button" class="btn btn-success">Add</button>
-              </router-link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    <router-view></router-view>
-  </div>
+  <router-view></router-view>
 </template>
-
-<script>
-
-
-export default {
-  name: 'App',
-}
-</script>
-
-<style>
-
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 import App from './App.vue'
-import router from './router'
+import router from "./router.js";
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
+const pinia = createPinia()
+
+app.use(ElementPlus, { size: 'small', zIndex: 3000 })
 app.use(router)
+app.use(pinia)
 app.mount('#app')
